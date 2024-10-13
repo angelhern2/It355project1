2eda84310097d5b78bb96ba61b6db9c36029de90
SMHUDS1 <smhuds1@ilstu.edu>
2024-10-13 15:34:54 -0500

    Solomons 9 Rules

--- solomoncodes/ResetOutputStream.java
+++ solomoncodes/ResetOutputStream.java
@@ -6,7 +6,7 @@ public class MyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.getWriter().write("Hello, World!");
-
+ 
         // Ensure no further modifications to the response after committing it
         // No further reset or modifications to the output stream are allowed after this point
     }
